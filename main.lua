5fb25e9761c902e7d4455be59a7a5e7dc854633c
Andrew Minnich <aminnich3@gmail.com>
2018-06-22 15:04:27 -0400

    basic full quantizing

--- main.lua
+++ main.lua
@@ -1,7 +1,14 @@
+-- https://github.com/rxi/lume/blob/master/lume.lua#L87
+local function round(x, increment)
+	if increment then return round(x / increment) * increment end
+	return x >= 0 and math.floor(x + .5) or math.ceil(x - .5)
+end
+
 local song = renoise.song()
 
 local notes = {}
 
+-- get notes
 for pos, col in song.pattern_iterator:note_columns_in_pattern(song.selected_pattern_index, true) do
 	if col.is_selected and not col.is_empty then
 		for _, note in ipairs(notes) do
@@ -20,9 +27,26 @@ for pos, col in song.pattern_iterator:note_columns_in_pattern(song.selected_patt
 	end
 end
 
+-- quantize notes
 for _, note in ipairs(notes) do
-	rprint(note)
-	rprint '\n'
+	note.start = round(note.start, 255)
+	note.finish = note.finish and round(note.finish, 255)
 end
 
-rprint 'done'
+-- clear selection
+for _, col in song.pattern_iterator:note_columns_in_pattern(song.selected_pattern_index, true) do
+	if col.is_selected then
+		col:clear()
+	end
+end
+
+-- write note data
+local pattern = song.selected_pattern
+for _, note in ipairs(notes) do
+	local start_line = math.floor(note.start / 255)
+	local finish_line = note.finish and math.floor(note.finish / 255)
+	pattern:track(note.track):line(start_line):note_column(note.column).note_value = note.value
+	if finish_line then
+		pattern:track(note.track):line(finish_line):note_column(note.column).note_value = 120
+	end
+end
