21cbd9a96e53ee6762346d156ee3380ede9fa678
Andrew Minnich <aminnich3@gmail.com>
2018-06-22 15:17:28 -0400

    split into functions

--- main.lua
+++ main.lua
@@ -6,47 +6,61 @@ end
 
 local song = renoise.song()
 
-local notes = {}
+local function clear(iterator)
+	for _, col in iterator do
+		if col.is_selected then
+			col:clear()
+		end
+	end
+end
 
--- get notes
-for pos, col in song.pattern_iterator:note_columns_in_pattern(song.selected_pattern_index, true) do
-	if col.is_selected and not col.is_empty then
-		for _, note in ipairs(notes) do
-			if not note.finish and note.track == pos.track and note.column == pos.column then
-				note.finish = pos.line * 255 + col.delay_value
+local function getIterator()
+	return song.pattern_iterator:note_columns_in_pattern(song.selected_pattern_index, true)
+end
+
+local function getNotes(iterator)
+	local notes = {}
+	for pos, col in iterator do
+		if col.is_selected and not col.is_empty then
+			for _, note in ipairs(notes) do
+				if not note.finish and note.track == pos.track and note.column == pos.column then
+					note.finish = pos.line * 255 + col.delay_value
+				end
+			end
+			if col.note_value ~= renoise.PatternLine.NOTE_OFF then
+				table.insert(notes, {
+					value = col.note_value,
+					track = pos.track,
+					column = pos.column,
+					start = pos.line * 255 + col.delay_value
+				})
 			end
 		end
-		if col.note_value ~= 120 then
-			table.insert(notes, {
-				value = col.note_value,
-				track = pos.track,
-				column = pos.column,
-				start = pos.line * 255 + col.delay_value
-			})
+	end
+	return notes
+end
+
+local function quantizeNotes(notes)
+	for _, note in ipairs(notes) do
+		note.start = round(note.start, 255)
+		note.finish = note.finish and round(note.finish, 255)
+	end
+end
+
+local function writeNotes(notes)
+	local pattern = song.selected_pattern
+	for _, note in ipairs(notes) do
+		local pattern_track = pattern:track(note.track)
+		local start_line = math.floor(note.start / 255)
+		local finish_line = note.finish and math.floor(note.finish / 255)
+		pattern_track:line(start_line):note_column(note.column).note_value = note.value
+		if finish_line then
+			pattern_track:line(finish_line):note_column(note.column).note_value = renoise.PatternLine.NOTE_OFF
 		end
 	end
 end
 
--- quantize notes
-for _, note in ipairs(notes) do
-	note.start = round(note.start, 255)
-	note.finish = note.finish and round(note.finish, 255)
-end
-
--- clear selection
-for _, col in song.pattern_iterator:note_columns_in_pattern(song.selected_pattern_index, true) do
-	if col.is_selected then
-		col:clear()
-	end
-end
-
--- write note data
-local pattern = song.selected_pattern
-for _, note in ipairs(notes) do
-	local start_line = math.floor(note.start / 255)
-	local finish_line = note.finish and math.floor(note.finish / 255)
-	pattern:track(note.track):line(start_line):note_column(note.column).note_value = note.value
-	if finish_line then
-		pattern:track(note.track):line(finish_line):note_column(note.column).note_value = 120
-	end
-end
+local notes = getNotes(getIterator())
+quantizeNotes(notes)
+clear(getIterator())
+writeNotes(notes)
