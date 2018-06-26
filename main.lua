2d66394f0e3ea94537622a88463a6faf5279a5a9
Andrew Minnich <aminnich3@gmail.com>
2018-06-26 02:34:33 -0400

    consolidate to one loop
    seems to work fine

--- main.lua
+++ main.lua
@@ -3,7 +3,9 @@ local util = require 'util'
 
 local song = renoise.song()
 local notes = iterator.get_notes(song, 'selection')
-for _, note in ipairs(notes) do note:quantize(1, 1, 'quantize_length') end
 iterator.clear_notes(song, 'selection')
-for _, note in ipairs(notes) do note:resolve_collisions(notes) end
-for _, note in ipairs(notes) do note:write() end
+for _, note in ipairs(notes) do
+	note:quantize(1, 1, 'quantize_length')
+	note:resolve_collisions(notes)
+	note:write()
+end
