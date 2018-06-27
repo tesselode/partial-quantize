c76d494f4b5821ebfaa2f692dd1296f9bf489989
Andrew Minnich <aminnich3@gmail.com>
2018-06-27 14:48:39 -0400

    keybindings

--- main.lua
+++ main.lua
@@ -170,3 +170,39 @@ renoise.tool():add_menu_entry {
 	name = 'Pattern Editor:Selection:Partial Quantize...',
 	invoke = function() create_gui(false, 'selection') end,
 }
+
+renoise.tool():add_keybinding {
+	name = 'Pattern Editor:Selection:Partially Quantize Selection',
+	invoke = function(repeated)
+		if not repeated then
+			create_gui(false, 'selection')
+		end
+	end,
+}
+
+renoise.tool():add_keybinding {
+	name = 'Pattern Editor:Column Operations:Partially Quantize Column',
+	invoke = function(repeated)
+		if not repeated then
+			create_gui(false, 'column')
+		end
+	end,
+}
+
+renoise.tool():add_keybinding {
+	name = 'Pattern Editor:Track Operations:Partially Quantize Track',
+	invoke = function(repeated)
+		if not repeated then
+			create_gui(false, 'track')
+		end
+	end,
+}
+
+renoise.tool():add_keybinding {
+	name = 'Pattern Editor:Pattern Operations:Partially Quantize Pattern',
+	invoke = function(repeated)
+		if not repeated then
+			create_gui(false, 'all_tracks')
+		end
+	end,
+}
